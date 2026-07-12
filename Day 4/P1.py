3ffa03314ece767e24bc4e3b7a085a786627d25e
Bhakti Chavan <bhaktiichavan3@gmail.com>
2026-07-12 10:15:43 +0530

    Impure and pure functions

---
+++ Day 4/P1.py
@@ -0,0 +1,15 @@
+
+counter = 0
+def impure_increment():
+    global counter
+    counter += 1
+    return counter
+
+# print(impure_increment())
+# print(impure_increment())
+
+def pure_increment(x):
+    return x+1
+
+print(pure_increment(5))
+print(pure_increment(5))
