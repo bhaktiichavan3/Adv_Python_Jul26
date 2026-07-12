8da81a85c661c765b406c78fb6ea640fd6d7853e
Bhakti Chavan <bhaktiichavan3@gmail.com>
2026-07-12 10:09:06 +0530

    Lambda function

---
+++ Day 3/P3.py
@@ -0,0 +1,16 @@
+add_lambda = lambda a,b:a+b
+print(add_lambda(10,50))
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+even_numbers = list(filter(lambda x: x%2==0,numbers))
+print(even_numbers)
+
+squared = list(map(lambda x:x**2,numbers))
+print(squared)
+
+student = {
+    'name':'Rakesh',
+    'age':36,
+    'grade': lambda x: f"Grade: {x}%"
+}
+print(student['grade'](91))
