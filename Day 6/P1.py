3bbea5537aabc07d5c54dd2ec02b40ecc5e73362
Bhakti Chavan <bhaktiichavan3@gmail.com>
2026-07-18 10:45:22 +0530

    Refresher: Loops

---
+++ Day 6/P1.py
@@ -0,0 +1,10 @@
+fruits = ['apple','banana','cherry']
+for fruit in fruits:
+    print(fruit)
+
+for i in range(5):
+    print(i)
+
+person = {'name':'Bhakti','age':20,'city':'Mumbai'}
+for key,value in person.items():
+    print(f"{key}:{value}")
