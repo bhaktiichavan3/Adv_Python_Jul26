fe5c2de565114f6dc5cb273b67e274f5f08254b1
Bhakti Chavan <bhaktiichavan3@gmail.com>
2026-07-18 10:29:46 +0530

    Decoratorsd

---
+++ Day 5/P4.py
@@ -0,0 +1,18 @@
+def simple_decorator(func):
+    def wrapper():
+        print("Before the function call")
+        func()
+        print("After the function call")
+
+    return wrapper
+
+def say_hello():
+    print("Hello world")
+
+decorated_hello = simple_decorator(say_hello)
+decorated_hello()
+
+@simple_decorator 
+def say_hi():
+    print("Hi there, Welcome")
+say_hi()
