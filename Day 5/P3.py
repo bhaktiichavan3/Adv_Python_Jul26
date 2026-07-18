54813bb767e19e64dd2eed2ac78c121c5d5a5ecc
Bhakti Chavan <bhaktiichavan3@gmail.com>
2026-07-18 10:29:20 +0530

    closure vs class

---
+++ Day 5/P3.py
@@ -0,0 +1,25 @@
+
+def make_counter():
+    count = 0
+    def increment():
+        # keyword used inside nested function to declare that a variable 
+        # belongs to nearest enclosing function scope
+        nonlocal count
+        count+=1
+        return count
+    return increment
+counter = make_counter()
+print(counter())
+print(counter())
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
+counter_obj = Counter()
+
+print(counter_obj.increment())
+print(counter_obj.increment())
